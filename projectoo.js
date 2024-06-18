844bd01b9cc9fa4194bd5e6444ad42d65b2665e1
syedzeeshan021 <159940398+syedzeeshan021@users.noreply.github.com>
2024-06-18 19:26:48 +0500

    Initial commit

---
+++ projectoo.js
@@ -0,0 +1,2 @@
+/*We write a Hello World to start our typescript learning process*/
+console.log("Hello, World!");
---
+++ projectoo.ts
@@ -0,0 +1,4 @@
+/*We write a Hello World to start our typescript learning process*/
+
+console.log("Hello, World!")
+
